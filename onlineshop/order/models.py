d4f5528ca0edfd7d2a7cdfb261ca6408cba1d381
mohamad hosein <mmdtrni@gmail.com>
2021-09-11 17:09:17 +0430

    update models

--- onlineshop/order/models.py
+++ onlineshop/order/models.py
@@ -6,12 +6,15 @@ from product.models import Product
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.RESTRICT)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-    # AVAILABLE = 'AVAILABLE'
-    # UNAVAILABLE = 'UNAVAILABLE'
-    # availability_choices = (
-    #     (AVAILABLE, 'AVAILABLE'),
-    #     (UNAVAILABLE, 'UNAVAILABLE')
-    # )
-    # availability = models.CharField(choices=availability_choices, max_length=100, default=AVAILABLE)
     count = models.PositiveIntegerField()
+
+    AVAILABLE = 'AVAILABLE'
+    UNAVAILABLE = 'UNAVAILABLE'
+    availability_choices = (
+        (AVAILABLE, 'AVAILABLE'),
+        (UNAVAILABLE, 'UNAVAILABLE')
+    )
+    availability = models.CharField(choices=availability_choices, max_length=100, default=AVAILABLE)
+
+    def __str__(self):
+        return f'{self.customer.first_name} {self.customer.last_name} | {self.product.name}'
