185e8bf1089715e4098126754816437b8b7cd93e
mohamad hosein <mmdtrni@gmail.com>
2021-09-18 04:10:42 +0430

    views

--- onlineshop/customer/api/views.py
+++ onlineshop/customer/api/views.py
@@ -1,8 +1,27 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from customer.models import Customer
+from customer.api.serializer import *
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 
 
+@api_view(['GET', 'POST'])
+def register(request):
+    if request.method == 'POST':
+        pass
+
+
+@api_view(['GET', 'POST'])
 def customerlist(request):
-    return None
+    if request.method == 'GET':
+        customer = get_list_or_404(Customer)
+        serializer = CustomerSerializer(customer, context={'request': request})
+        return Response(serializer.data)
 
 
+@api_view(['GET', 'POST'])
 def customerdetail(request, pk):
     return None
+
+
+
--- onlineshop/customer/views.py
+++ onlineshop/customer/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from customer.forms import *
 
-# Create your views here.
+
+def signup(request):
+    context = dict()
+    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            customer = form.save(commit=False)
+            if Customer.objects.filter(id=customer.id):
+                form = SignupForm()
+
+            else:
+                customer.save()
+
+                return render(request, 'customer/login.html', context)
+    context['form'] = form
+    return render(request, 'customer/signup.html', context)
+
+
+def login(request):
+    pass
