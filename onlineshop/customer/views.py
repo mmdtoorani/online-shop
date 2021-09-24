77b02339d619e3826150d6286defaffcdce1789d
mohamad hosein <mmdtrni@gmail.com>
2021-09-25 02:44:55 +0330

    comment

--- onlineshop/customer/views.py
+++ onlineshop/customer/views.py
@@ -2,23 +2,19 @@ from django.shortcuts import render
 from customer.forms import *
 
 
-def signup(request):
-    context = dict()
-    form = SignupForm()
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            customer = form.save(commit=False)
-            if Customer.objects.filter(id=customer.id):
-                form = SignupForm()
-
-            else:
-                customer.save()
-
-                return render(request, 'customer/login.html', context)
-    context['form'] = form
-    return render(request, 'customer/signup.html', context)
-
-
-def login(request):
-    pass
+# def signup(request):
+#     context = dict()
+#     form = SignupForm()
+#     if request.method == 'POST':
+#         form = SignupForm(request.POST)
+#         if form.is_valid():
+#             customer = form.save(commit=False)
+#             if Customer.objects.filter(id=customer.id):
+#                 form = SignupForm()
+#
+#             else:
+#                 customer.save()
+#
+#                 return render(request, 'customer/../account/templates/login.html', context)
+#     context['form'] = form
+#     return render(request, 'customer/../account/templates/signup.html', context)
