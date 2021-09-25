add160b873199679db22792e97ec5b409e4afb6b
mohamad hosein <mmdtrni@gmail.com>
2021-09-25 04:57:10 +0330

    fix urls

--- onlineshop/customer/urls.py
+++ onlineshop/customer/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from customer import views
 
 urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+
 ]
--- onlineshop/product/urls.py
+++ onlineshop/product/urls.py
@@ -4,5 +4,4 @@ from product import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
 ]
