9b7cddeca3862e3f1ed383776f123cb4da290aa5
mohamad hosein <mmdtrni@gmail.com>
2021-09-25 05:01:29 +0330

    some changes

--- onlineshop/customer/views.py
+++ onlineshop/customer/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-from customer.forms import *
 
 
-# def signup(request):
-#     context = dict()
-#     form = SignupForm()
-#     if request.method == 'POST':
-#         form = SignupForm(request.POST)
-#         if form.is_valid():
-#             customer = form.save(commit=False)
-#             if Customer.objects.filter(id=customer.id):
-#                 form = SignupForm()
-#
-#             else:
-#                 customer.save()
-#
-#                 return render(request, 'customer/../account/templates/login.html', context)
-#     context['form'] = form
-#     return render(request, 'customer/../account/templates/signup.html', context)
+def login(request):
+    print("request ta inja reside")
+    return render(request, 'customer/login.html', {'request': request})
+
+
+def signup(request):
+    return render(request, 'customer/signup.html', {'request': request})
