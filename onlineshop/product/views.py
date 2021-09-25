870aa6ba96b36f63b16a8d9be38731d81a121ce7
mohamad hosein <mmdtrni@gmail.com>
2021-09-25 09:22:07 +0330

    home view

--- onlineshop/product/views.py
+++ onlineshop/product/views.py
@@ -1,8 +1,23 @@
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 
 from customer.models import Customer
 
 
 def home(request):
-    return render(request, 'product/home.html', {'request': request})
+    if request.method == 'GET':
+        return render(request, 'product/home.html', {'request': request})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if str(Customer.objects.all().get(username=username)) == username:
+            if Customer.objects.get(username=username).password == password:
+                print('rad shod')
+                return render(request, 'product/home.html', {'request': request})
+            else:
+                return HttpResponse('<h1>password ehtebah</h1>')
+        else:
+            return HttpResponse('<h1>username eshtebah</h1>')
+
