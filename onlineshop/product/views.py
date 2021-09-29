f1136745f5177fa4fb5c8f65a65a322681a0c461
mohamad hosein <mmdtrni@gmail.com>
2021-09-29 06:20:14 +0330

    add an error message for login.html

--- onlineshop/product/views.py
+++ onlineshop/product/views.py
@@ -2,8 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-
-from customer.models import Customer
+from django.contrib import messages
 
 
 def home(request):
@@ -14,21 +13,13 @@ def home(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
-        # print(request.POST['password2'])
-        print('user authenticate shod!!!')
         if user:
-            print('user is not None!!!')
             login(request, user)
-            print('user has been login')
             return render(request, 'product/home.html', {'request': request})
+        else:
+            message = 'username or password is not correct!'
+            messages.error(request, message)
+            return redirect('customer:login')
 
-        # if Customer.objects.get(username=username).username == username:
-        #     if Customer.objects.get(username=username).password == password:
-        #         print('rad shod')
-        #         login()
-        #         return render(request, 'product/home.html', {'request': request})
-        #     else:
-        #         return HttpResponse('<h1>password ehtebah</h1>')
-        # else:
-        #     return HttpResponse('<h1>username eshtebah</h1>')
-
+    else:
+        return render(request, 'product/home.html', {'request': request})
