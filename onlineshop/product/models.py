73a345d0d146fd39a6031082853b086dec68189c
mohamad hosein <mmdtrni@gmail.com>
2021-09-30 04:00:42 +0330

    some changes for order and product models

--- onlineshop/product/models.py
+++ onlineshop/product/models.py
@@ -9,19 +9,22 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
     product_name = models.CharField(max_length=80)
     percent = models.PositiveIntegerField(default=0, blank=True)
     initial_price = models.PositiveIntegerField(default=0, blank=True)
-    final_price = models.PositiveIntegerField(default=0)
+    final_price = models.PositiveIntegerField(default=0, blank=True)
+    stock = models.SmallIntegerField(default=0)
     description = models.TextField()
     photo = models.ImageField(upload_to='./product/media', null=True, blank=True)
-    stock = models.SmallIntegerField(default=0)
 
     def __str__(self):
         return self.product_name
 
     @property
     def generate_final_price(self):
-        self.final_price = self.initial_price - (self.initial_price * self.percent)
+        """
+        final_price = count of product * Result of initial price deduction from discount percent
+        """
+        self.final_price = self.stock * (self.initial_price - (self.initial_price * (self.percent / 100)))
         return self.final_price
