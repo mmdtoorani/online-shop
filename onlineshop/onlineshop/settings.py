ba3c8b1dc3f9d3c5f8f0782d9e485126bb40ed9e
mohamad hosein <mmdtrni@gmail.com>
2021-10-01 03:51:05 +0330

    install cors headers

--- onlineshop/onlineshop/settings.py
+++ onlineshop/onlineshop/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'phonenumber_field',
+    'corsheaders',
     'bootstrap4',
     'account',
     'customer',
@@ -124,6 +125,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+]
 
 # REST_FRAMEWORK = {
 #     "DEFAULT_PERMISSION_CLASSES": [
