c778bacc04ba3a12fa0e0181c2e8831890505f33
mohamad hosein <mmdtrni@gmail.com>
2021-10-11 15:34:47 +0330

    media setting

--- onlineshop/onlineshop/urls.py
+++ onlineshop/onlineshop/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('product.urls', 'product')),
     path('admin/', admin.site.urls),
     path('api/', include('core.api.urls')),
     path('customer/', include('customer.urls', 'customer'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
