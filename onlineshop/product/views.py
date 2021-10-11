542a7b3f2835a67c4d07db33901809d0b90132ad
mohamad hosein <mmdtrni@gmail.com>
2021-10-11 15:37:01 +0330

    impermanent commit

--- onlineshop/product/views.py
+++ onlineshop/product/views.py
@@ -2,11 +2,10 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
+from product.models import Category, Product
+
 
 def home(request):
-    if request.method == 'GET':
-        return render(request, 'product/home.html', {'request': request})
-
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -19,5 +18,12 @@ def home(request):
             messages.error(request, message)
             return redirect('customer:login')
 
-    else:
-        return render(request, 'product/home.html', {'request': request})
+    return render(request, 'product/home.html', {'request': request})
+
+
+# def category(request):
+#     if request.method == 'GET':
+#         context = dict()
+#         print(request.GET)
+#         # products = Product.objects.filter(category=request)
+#         return render(request, 'product/categorized_products.html', context=context)
