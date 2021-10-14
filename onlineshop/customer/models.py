f6db2d96b44c67ca693712ace773c4674553db06
mohamad hosein <mmdtrni@gmail.com>
2021-10-14 05:58:33 +0330

    delete last version logic

--- onlineshop/customer/models.py
+++ onlineshop/customer/models.py
@@ -6,15 +6,8 @@ from account.models import Account
 
 
 class Customer(User):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
-    # username = models.CharField(max_length=100, unique=True)
-    # first_name = models.CharField(max_length=20, null=True, blank=True)
-    # last_name = models.CharField(max_length=20, null=True, blank=True)
-    # email = models.EmailField(max_length=100, unique=True)
-    phone = PhoneNumberField(null=False, blank=False)
+    phone = PhoneNumberField(null=True, blank=True)
     address = models.TextField(max_length=400, null=True, blank=True)
-    # password = models.CharField(max_length=20, null=False)
-    # password2 = models.CharField(max_length=20, null=False)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
