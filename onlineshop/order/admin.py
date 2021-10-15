461be8462ec3f2c44465c444ff9265ba55a3cea6
mohamad hosein <mmdtrni@gmail.com>
2021-10-15 03:49:28 +0330

    fix order's bug

--- onlineshop/order/admin.py
+++ onlineshop/order/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from order.models import Order
+from order.models import Order, OrderItem
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['customer', 'product', 'stock', 'availability', ]
+    pass
+    # list_display = ['customer', 'product', 'stock', 'availability', ]
     # list_filter = ['customer']
-    list_editable = ['stock', 'availability', ]
+    # list_editable = ['stock', 'availability', ]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
--- onlineshop/order/models.py
+++ onlineshop/order/models.py
@@ -4,7 +4,7 @@ from product.models import Product
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     # product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     AVAILABLE = 'AVAILABLE'
@@ -20,6 +20,6 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     stock = models.PositiveIntegerField(default=0)
