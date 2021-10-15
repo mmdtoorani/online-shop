d828a7650807de1b9c0576d2d1a2d6e7358fa753
mohamad hosein <mmdtrni@gmail.com>
2021-10-15 09:13:06 +0330

    try for test

--- onlineshop/customer/tests.py
+++ onlineshop/customer/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Customer
 
-# Create your tests here.
+
+class TestCustomerModel(TestCase):
+    def set_up(self):
+        self.customer = Customer.objects.create_user(
+            username='akbar',
+            first_name='akbar',
+            last_name='akbari',
+            phone='+9123856956',
+            email='akkbar@gmail.com',
+            password='svd432d5b1gq23w54ld',
+        )
+
+    def test_is_created(self):
+        self.assertEqual(self.customer.last_name, 'akbari')
