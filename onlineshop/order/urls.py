c2635ed216aef756346a9be7615c90eeed6b1ef4
mohamad hosein <mmdtrni@gmail.com>
2021-10-22 01:44:21 +0330

    order urls

--- onlineshop/order/urls.py
+++ onlineshop/order/urls.py
@@ -5,5 +5,9 @@ from order import views
 app_name = 'order'
 
 urlpatterns = [
+    path('single_product/', views.single_product, name='single_product'),
     path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart, name='cart'),
+    path('delete_from_cart/<int:product_id>/', views.delete_from_cart, name='delete_from_cart'),
+    path('checkout/', views.checkout, name='checkout')
 ]
